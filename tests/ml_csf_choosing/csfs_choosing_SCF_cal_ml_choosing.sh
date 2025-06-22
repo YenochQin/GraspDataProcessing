94291dfbaa787d8e5d8cb15252905b2bf09b0ea1
YenochQin <yenochqin@hotmail.com>
2025-06-22 13:07:00 +0800

    20250622 ws3 update

--- tests/ml_csf_choosing/csfs_choosing_SCF_cal_ml_choosing.sh
+++ tests/ml_csf_choosing/csfs_choosing_SCF_cal_ml_choosing.sh
@@ -1,18 +1,18 @@
 #!/bin/zsh
-#SBATCH -J GdIoddImlcias3_odd1
+#SBATCH -J GdIoddImlcias4_odd2
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=46
 #SBATCH -p work3
-#SBATCH --output=as3oddImlci.log
+#SBATCH --output=as4_odd2GdIoddImlci.log
 . /usr/share/Modules/init/zsh
 ###########################################
 # mpi run CPU core
 processor=46
 ###########################################
 ## module load
-module load mpi/openmpi-x86_64-gcc   
+module load mpi/openmpi-x86_64-gcc
 module load openblas/0.3.28-gcc-11.4.1
-module load grasp/grasp_openblas 
+module load grasp/grasp_openblas
 ###########################################
 # ⚠️ 关键修改：确保正确加载 Conda（zsh 需要手动初始化）
 source /home/workstation3/AppFiles/miniconda3/etc/profile.d/conda.sh  || {
@@ -29,10 +29,10 @@ echo "True" > run.input
 ###########################################
 ## configuration
 atom=GdI
-conf="cv4odd1as3_odd1"
+conf="cv4odd1as4_odd2"
 varied="3"
-rwfnestimate_file="mJ-1-90chosenas3_odd1.w"
-
+loop1_rwfn_file="mJ-1-90chosenas3_odd2.w"
+rwfnestimate_file=${conf}_1.w
 ###########################################
 # 检查 Python 路径
 which python
@@ -49,12 +49,11 @@ if [ $loop -eq 1 ]; then
 fi
 ###########################################
 cal_status=$(csfs_ml_choosing_config_load.py get continue_cal)
-if [[ "$cal_status" == "False" ]]; then
+if [[ "$cal_status" == "false" ]]; then
     echo "================计算终止================"
     break
 fi
 ###########################################
-
 ## 组态选择处理
 echo "================执行组态选择================"
 python choosing_csfs.py 
@@ -62,77 +61,107 @@ if [ $? -ne 0 ]; then
     echo "组态选择失败!"
     exit 1
 fi
-
+###########################################
 ## grasp calculation routine
-# mkdir ${conf}_${loop}
+
 cd ${conf}_${loop}
 mkdisks ${processor} caltmp
 ### 1. rcsf
-# cp ${conf}_${loop}.c rcsf.inp # rmcdhf
+cp ${conf}_${loop}.c rcsf.inp # rmcdhf
 cp ../isodata .
-cp ../${rwfnestimate_file} ${conf}_${loop}.w
 
-# ### 2. rangular
-# mpirun -np ${processor} rangular_mpi <<EOF
-# y
-# EOF
+if [ $loop -eq 1 ]; then
+echo "================第一次循环，使用${loop1_rwfn_file}================"
+cp ../${loop1_rwfn_file} ${conf}.w
+else
+echo "================第${loop}次循环，使用${rwfnestimate_file}================"
+cp ../${rwfnestimate_file} ${conf}.w
+fi
 
-# ### 3.rwfnestimate
-# rwfnestimate << EOF
-# y
-# 1
-# ${rwfnestimate_file}
-# *
-# 2
-# *
-# 3
-# *
-# EOF
-
-# ### 4. rmcdhf
-# mpirun -np ${processor} rmcdhf_mem_mpi <<EOF
-# y
-# 1-3
-# 5
-
-
-# 100
-# EOF
-
-# ### 5. rsave
-# rsave ${conf}_${loop}
-
-# 5. rci
-mpirun -np ${processor} rci_mpi <<EOF
+### 2. rangular
+mpirun -np ${processor} rangular_mpi <<EOF
 y
-${conf}_${loop}
-y
-y
-1.d-6
-y
-n
-n
-y
-5
-1-2
 EOF
 
-# ### 6. jj2lsj rmcdhf
-# jj2lsj << EOF
+if [ $loop -eq 1 ]; then
+### 3.rwfnestimate
+rwfnestimate << EOF
+y
+1
+${loop1_rwfn_file}
+*
+2
+*
+3
+*
+EOF
+
+### 4. rmcdhf - 首次循环使用特定轨道参数
+orbital_params="10s,9p,8d,7f,6g"
+else
+### 3.rwfnestimate (与上面相同)
+rwfnestimate << EOF
+y
+1
+${loop1_rwfn_file}
+*
+2
+*
+3
+*
+EOF
+
+### 4. rmcdhf - 后续循环不使用轨道参数
+orbital_params=""
+fi
+
+### 4. rmcdhf (统一执行)
+mpirun -np ${processor} rmcdhf_mem_mpi <<EOF
+y
+1-4
+5
+${orbital_params}
+
+100
+EOF
+### 5. rsave
+rsave ${conf}_${loop}
+
+if [ $loop -eq 1 ]; then
+cp ${conf}_${loop}.w ..
+fi
+# # 5. rci
+# mpirun -np ${processor} rci_mpi <<EOF
+# y
 # ${conf}_${loop}
+# y
+# y
+# 1.d-6
+# y
+# n
 # n
 # y
-# y
+# 5
+# 1-4
 # EOF
-### 6. jj2lsj rci
+
+### 6. jj2lsj rmcdhf
 jj2lsj << EOF
 ${conf}_${loop}
-y
+n
 y
 y
 EOF
+# ### 6. jj2lsj rci
+# jj2lsj << EOF
+# ${conf}_${loop}
+# y
+# y
+# y
+# EOF
 ### 7. generate energy levels data file
-rlevels ${conf}_${loop}.cm > ${conf}_${loop}.level
+rlevels ${conf}_${loop}.m > ${conf}_${loop}.level # rmcdhf
+# rlevels ${conf}_${loop}.cm > ${conf}_${loop}.level # rci
 cd ..
 
 ## 机器学习训练
